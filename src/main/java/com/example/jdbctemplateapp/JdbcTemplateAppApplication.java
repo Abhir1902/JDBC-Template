7fbf803b5da6e69320f6b551d5baea59b8d4df5d
Abhir20BCE1076 <abhirmangesh.mirikar2020@vitstudent.ac.in>
2023-05-23 07:38:54 +0530

    added new files

---
+++ src/main/java/com/example/jdbctemplateapp/JdbcTemplateAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbctemplateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTemplateAppApplication.class, args);
+	}
+
+}
